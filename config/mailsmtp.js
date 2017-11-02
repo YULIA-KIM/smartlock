a7cfaa7ebfd67c3e202427e8a30b034b317e85b7
nirvana6691@gmail.com <nirvana6691@gmail.com>
2017-11-02 21:54:55 +0900

    commit

--- config/mailsmtp.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: "smartlock.bc@gmail.com", // generated ethereal user
-        pass: "smartlock"  // generated ethereal password
-    }
-}
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/users.controller');
-
-router.post('/signUp', controller.signUp);
-router.post('/login', controller.login);
-router.get('/email_verify', controller.emailVerify);
-
-module.exports = router;
